12f9a4a1ee4ea5755b52f4879e168e2169a6e66f
Pedro Motta <pedromotta@outlook.com>
2016-05-02 21:27:43 -0300

    eslint e melhoria para pegar os dados

--- app/info/info.controller.js
+++ app/info/info.controller.js
@@ -5,32 +5,70 @@
     .module('localDockerApp')
     .controller('InfoController', InfoController);
 
-    InfoController.$inject = ['$http'];
+  InfoController.$inject = ['$http'];
 
-    function InfoController($http) {
-      var vm = this;
-      vm.containers = [];
-      $http
+  function InfoController($http) {
+    var vm = this;
+    vm.containers = [];
+    $http
       .get('/api/info')
       .then(infoResult, infoErrorResult);
 
-      function infoResult(result){
-        console.log(result);
-        if(result.data && result.data.length > 0){
-          result.data.forEach(function(item){
-            var container = {
-              name: item.Names[0] ? item.Names[0] : '',
-              state : item.State ? item.State : '',
-              status: item.Status ? item.Status : '',
-              port: item.Ports[0] ? item.Ports[0] : '',
-              version : item.Version ? item.Version : ''
-            }
+    function infoResult(result) {
+      if (result.data && result.data.length > 0) {
+        result.data.forEach(function (item) {
+          var container = buildContainerObj(item);
           vm.containers.push(container);
-          })
+        });
+      }
+    }
+
+    function getName(data) {
+      var name = '';
+      var nameParts;
+      var fullNameParts = data.Image.split(':');
+
+      if (fullNameParts.length) {
+        nameParts = fullNameParts[0].split('/');
+        if (nameParts.length > 1) {
+          name = nameParts[1];
+        } else if (nameParts.length) {
+          name = nameParts[0];
         }
       }
 
-      function infoErrorResult(err){
-      }
+      return name;
     }
+
+    function getPort(data) {
+      var port = '';
+      var ports = data.Ports;
+      if (ports && ports[0]) {
+        port = ports[0].PublicPort;
+      }
+      return port;
+    }
+
+    function getVersion(data) {
+      var version = '-';
+      var imageParts = data.Image.split(':');
+
+      if (imageParts.length === 2) {
+        version = imageParts[1];
+      }
+      return version;
+    }
+
+    function buildContainerObj(data) {
+      return {
+        name: getName(data),
+        state: data.State || '',
+        status: data.Status || '',
+        port: getPort(data),
+        version: getVersion(data)
+      };
+    }
+
+    function infoErrorResult() {}
+  }
 })();
