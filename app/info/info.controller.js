55f08f28dbae2831c6c8deb1bbf2d35672852d04
Daniel <danieldepaulaa@gmail.com>
2016-05-03 23:35:15 -0300

    toolbar com logo e ordenação dos itens pelo filtro do angular

--- app/info/info.controller.js
+++ app/info/info.controller.js
@@ -30,17 +30,6 @@
           var container = buildContainerObj(item);
           vm.containers.push(container);
         });
-        vm.containers.sort(compare);
-      }
-    }
-
-    function compare(a,b) {
-      if (a.name < b.name) {
-        return -1;
-      } else if (a.name > b.name) {
-        return 1;
-      } else {
-        return 0;
       }
     }
 
